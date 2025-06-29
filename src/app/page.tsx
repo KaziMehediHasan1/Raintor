e62769f7e72ac2deb8500a74d194da3bae5c0370
KaziMehediHasan1 <kazimehedihasan243@gmail.com>
2025-06-29 18:20:17 +0600

    add logo and wrapper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
+import Wrapper from "@/components/shared/Wrapper";
 import React from "react";
 
 const Home = () => {
   return (
     <div>
-      <p className="font-montserrat bg-yellow-950 text-4xl font-semibold text-white">Home</p>
-      <p className="bg-amber-200 text-2xl">Home2</p>
+      <Wrapper>
+        <p className="font-montserrat bg-yellow-950 text-4xl font-semibold text-white ">
+          Home
+        </p>
+        <p className="bg-amber-200 text-2xl">Home2</p>
+      </Wrapper>
     </div>
   );
 };
--- src/components/shared/Wrapper.tsx
+++ src/components/shared/Wrapper.tsx
@@ -1,7 +1,7 @@
 import { IWrapper } from "@/types";
 
 const Wrapper = ({ children }: IWrapper) => {
-  return <div>{children}</div>;
+  return <div className="max-w-[1440px] w-full p-2 mx-auto">{children}</div>;
 };
 
 export default Wrapper;
